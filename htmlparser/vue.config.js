72f4c547979da165a0718ae8130f446424c27c3b
suhoKKK <k8325172@gmail.com>
2024-04-29 02:17:24 +0900

    불필요한 주석, 코드 삭제

--- htmlparser/vue.config.js
+++ htmlparser/vue.config.js
@@ -6,38 +6,12 @@ module.exports = defineConfig({
   }
 })
 
-// module.exports = {
-//   // devServer: {
-//   //   proxy: 'http://localhost:9090'
-//   // }
-// //   server: {
-// //     proxy: {
-// //         '/api': 'http://localhost:9090'
-// //     }
-// // }
-
-// devServer: {
-//   proxy: {
-//     '/api/': { // /api/로 시작하는 url은 아래의 전체 도메인을 추가하고, 옵션을 적용 
-//       target: 'http://localhost:9090', // 클라이언트에서 api로 보내는 요청은 주소를 3095로 바꿔서 보내겠다 라는 뜻
-//       changeOrigin: true, // cross origin 허용 설정
-//       pathRewrite: {
-//         '^api': ''
-//       }
-//     },
-//   },
-// },
-// };
 module.exports = {
   devServer: {
     proxy: {
       '/api': {
         target: 'http://localhost:9090', // Spring Boot 서버의 포트
-        // ws: true,
         changeOrigin: true,
-        // pathRewrite: {
-        //   '^/api': '',
-        // },
       },
     },
   },
